70a34064f9c7ca0bc4bd008011a73ec37094ffda
tamim <tamimyousefi023@gmail.com>
2025-04-13 11:54:07 +0330

    Q1 - explanation

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+// allowing specific type literals and custom string
+
 type TActions = "edit" | "create" | "delete" | "add";
 type TUtility<T extends string> = T | (string & {});
 function tableActionHandler<T extends TUtility<TActions>>(action:  T): void {
